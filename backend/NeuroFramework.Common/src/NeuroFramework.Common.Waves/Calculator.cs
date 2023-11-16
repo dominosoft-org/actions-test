65ee52716b73ee8234164805c63734aabe1edab5
Satoshi Uenoyama <52275228+uenoyama-dominosoft@users.noreply.github.com>
2023-11-16 19:09:20 +0900

    fulltest

---
+++ backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace NeuroFramework.Common.Waves {
+  public static class Calculator {
+    public static int Add(int x, int y) {
+      if (x == 50) {
+        return y;
+      }
+      return x + y;
+    }
+  }
+}
---
+++ backend/NeuroFramework.Common/tests/NeuroFramework.Common.Waves.Tests/CalculatorTest.cs
@@ -0,0 +1,9 @@
+﻿namespace NeuroFramework.Common.Waves.Tests;
+
+[TestClass]
+public class CalculatorTest {
+  [TestMethod]
+  public void AddTest() {
+    Assert.AreEqual(3, Calculator.Add(1, 2));
+  }
+}

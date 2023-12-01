d3026733c03b2b96e0ec86f1098923354428f159
nct-ds <77710137+nct-ds@users.noreply.github.com>
2023-12-01 14:29:34 +0900

    NPK2-1049 実験

--- backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
+++ backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
@@ -4,56 +4,6 @@
       if (x == 100) {
         return y;
       }
-      if (x == 101) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 102) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 103) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 104) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 105) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 106) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 107) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 108) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 109) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 111) {
-        x--;
-      } else {
-        x++;
-      }
       return x + y;
     }
   }
--- backend/NeuroFramework.Common/tests/NeuroFramework.Common.Waves.Tests/CalculatorTest.cs
+++ backend/NeuroFramework.Common/tests/NeuroFramework.Common.Waves.Tests/CalculatorTest.cs
@@ -4,57 +4,6 @@
 public class CalculatorTest {
   [TestMethod]
   public void AddTest() {
-    int x = Calculator.Add(1, 2);
-      if (x == 101) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 102) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 103) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 104) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 105) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 106) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 107) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 108) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 109) {
-        x--;
-      } else {
-        x++;
-      }
-      if (x == 111) {
-        x--;
-      } else {
-        x++;
-      }
-    Assert.AreEqual(3, x);
+    Assert.AreEqual(3, Calculator.Add(1, 2));
   }
 }

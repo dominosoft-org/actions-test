b328af1ca2b469cc3cdc52968d31759ff03b97f0
nct-ds <77710137+nct-ds@users.noreply.github.com>
2023-12-01 14:35:55 +0900

    NPK2-1049 実験

--- backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
+++ backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
@@ -1,7 +1,7 @@
 ﻿namespace NeuroFramework.Common.Waves {
   public static class Calculator {
     public static int Add(int x, int y) {
-      if (x == 100) {
+      if (x == 50) {
         return y;
       }
       return x + y;

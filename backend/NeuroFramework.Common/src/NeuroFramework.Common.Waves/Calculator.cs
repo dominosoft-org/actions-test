2312e7d069119e969c3cd2a9f911eb437493c934
nct-ds <77710137+nct-ds@users.noreply.github.com>
2023-12-01 11:33:46 +0900

    NPK2-1049 実験

--- backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
+++ backend/NeuroFramework.Common/src/NeuroFramework.Common.Waves/Calculator.cs
@@ -4,6 +4,56 @@
       if (x == 100) {
         return y;
       }
+      if (x == 101) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 102) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 103) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 104) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 105) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 106) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 107) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 108) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 109) {
+        x--;
+      } else {
+        x++;
+      }
+      if (x == 110) {
+        x--;
+      } else {
+        x++;
+      }
       return x + y;
     }
   }
